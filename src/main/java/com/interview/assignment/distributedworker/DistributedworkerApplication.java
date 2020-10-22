1925e4b59d6963a92449da053009d22687af547b
user <user@user.com>
2020-10-22 17:06:26 +0100

    initial commit

---
+++ src/main/java/com/interview/assignment/distributedworker/DistributedworkerApplication.java
@@ -0,0 +1,13 @@
+package com.interview.assignment.distributedworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedworkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/assignment/distributedworker/DistributedworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.assignment.distributedworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
