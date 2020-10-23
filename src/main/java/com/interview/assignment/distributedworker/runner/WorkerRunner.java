f2aa6a53cf36cef12408368e399dd741af757ffc
user <user@user.com>
2020-10-23 23:40:41 +0100

    workers business logic

---
+++ src/main/java/com/interview/assignment/distributedworker/runner/WorkerRunner.java
@@ -0,0 +1,84 @@
+package com.interview.assignment.distributedworker.runner;
+
+import com.interview.assignment.distributedworker.model.WorkerModel;
+import com.interview.assignment.distributedworker.service.HttpService;
+import com.interview.assignment.distributedworker.service.WorkerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+@Profile("!test")
+@Component
+public class WorkerRunner implements CommandLineRunner {
+
+    @Autowired
+    WorkerService workerService;
+
+    @Autowired
+    HttpService httpService;
+
+    Logger logger = LogManager.getLogger(WorkerRunner.class);
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        boolean repeat = true;
+        do {
+           repeat =  runLoop();
+
+        } while(repeat);
+        logger.info("No more jobs. Goodbye");
+    }
+
+    private boolean runLoop(){
+        int jobId = -1;
+        try {
+            jobId = workerService.getJobID();
+            logger.info(jobId == -1 ? "" : "JobId : " + jobId);
+
+        } catch (Exception e){
+            logger.info("Something went wrong running the stored function, check your database.");
+            return false;
+        }
+
+        if(jobId == -1){
+            //IF I didn't find any jobs I exit here.
+            return false;
+        }
+
+        WorkerModel model = null;
+        long id = jobId;
+
+        try {
+             model = workerService.findById(id);
+             String url = model.getUrl();
+             Map<String, String> response = httpService.fetch(url);
+             String http_code = response.get("http_code");
+
+             if (http_code.equals("ERROR")) {
+                 //If httpService threw and exception...
+                 model.setStatus("ERROR");
+                 model.setHttp_code(null);
+
+             } else {
+                 model.setStatus("DONE");
+                 model.setHttp_code(http_code);
+             }
+
+        } catch (Exception e){
+            //if there was an exception inside this whole with something else thing I handle it here
+            model.setStatus("ERROR");
+            model.setHttp_code(null);
+
+        } finally {
+            workerService.save(model);
+        }
+
+        return true;
+    }
+}
