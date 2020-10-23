a8c8781e7bcc1fd284ff3e9d494d6d1bb5bd6f68
user <user@user.com>
2020-10-23 23:38:28 +0100

    the app entry point

--- src/main/java/com/interview/assignment/distributedworker/DistributedworkerApplication.java
+++ src/main/java/com/interview/assignment/distributedworker/DistributedworkerApplication.java
@@ -2,8 +2,11 @@ package com.interview.assignment.distributedworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class DistributedworkerApplication {
 
 	public static void main(String[] args) {
